e5d1648c37774e2fb13113e7e1711123adc42e5d
Michael Budnick <michaeldbudnick@gmail.com>
2015-10-07 11:36:45 -0400

    Add Rspecs for two_sum and my_transpose

--- array_exercises_tdd/lib/array_exercises.rb
+++ array_exercises_tdd/lib/array_exercises.rb
@@ -5,6 +5,29 @@ class Array
     end
   end
 
+  def two_sum
+    output = []
+    self[0...length].each_with_index do |el, index|
+      self[index+1...length].each_with_index do |el2, sec|
+        second_index = sec + index + 1
+        output << [index, second_index] if el + el2 == 0
+      end
+    end
 
+    output
+  end
 
+  def my_transpose
+    return self if count <= 1
+
+    result = Array.new(self.first.length) { Array.new(self.length) }
+
+    length.times do |row_indx|
+      self[row_indx].length.times do |col_indx|
+        result[col_indx][row_indx] = self[row_indx][col_indx]
+      end
+    end
+
+    result
+  end
 end
--- array_exercises_tdd/spec/array_exercises_spec.rb
+++ array_exercises_tdd/spec/array_exercises_spec.rb
@@ -6,16 +6,78 @@ describe Array do
   describe "#my_uniq" do
     let(:arr) { [1,1,2,2] }
 
-    it "#my_uniq returns an array of uniq values" do
-      expect(arr.my_uniq).to eq(arr.uniq)
+    it "returns an empty array if given an empty array" do
+      expect([].my_uniq).to eq([])
     end
 
+    it "returns single element array" do
+      expect([1].my_uniq).to eq([1])
+    end
+
+    it "#my_uniq returns an array of uniq values" do
+      expect(arr.my_uniq).to eq([1,2])
+    end
+
+    it "does not modify original" do
+      expect{arr.my_uniq}.to_not change{arr}
+    end
   end
 
   describe "#two_sum" do
     let(:arr) { [-1, 0, 2, -2, 1] }
 
-    
+    it "returns an empty array if given an empty array" do
+      expect([].two_sum).to eq([])
+    end
+
+    it "returns single element array" do
+      expect([1].two_sum).to eq([])
+    end
+
+    it "finds all pairs of positions where the elements at those positions sum to zero" do
+      expect(arr.two_sum).to eq([[0, 4], [2, 3]])
+    end
+
+    it "does not modify original" do
+      expect{arr.two_sum}.to_not change{arr}
+    end
+  end
+
+  describe "#transpose" do
+    let(:arr) { [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+      ] }
+
+    let(:rect_arr) { [
+      [1, 2, 3],
+      [4, 5, 6]
+      ] }
+
+    it "returns an empty array if given an empty array" do
+      expect([].my_transpose).to eq([])
+    end
+
+    it "returns single element array" do
+      expect([1].my_transpose).to eq([1])
+    end
+
+    it "transposes a square matrix" do
+      expect(arr.my_transpose).to eq([[1, 4, 7],
+                                      [2, 5, 8],
+                                      [3, 6, 9]])
+    end
+
+    it "transposes a rectangular matrix" do
+      expect(rect_arr.my_transpose).to eq([[1, 4],
+                                           [2, 5],
+                                           [3, 6]])
+    end
+
+    it "does not modify original" do
+      expect{arr.my_transpose}.to_not change{arr}
+    end
   end
 
 end
